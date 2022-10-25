f544f004de26c9a262b06d5669550f9e7202dc81
Doc077 <dom.rajkowski@gmail.com>
2022-10-25 21:19:54 +0200

    Add 'new' command

---
+++ src/commands/new.command.ts
@@ -0,0 +1,14 @@
+import { logInfo } from '../utils/log-info.function';
+import { runCommand } from '../utils/run-command.function';
+import { Command } from '../decorators/command.decorator';
+
+@Command({
+  signature: 'new',
+})
+export class NewCommand {
+  public async handle(projectName: string): Promise<void> {
+    logInfo('Running database migrations...');
+
+    runCommand(`northle new ${projectName}`, { showOutput: true });
+  }
+}
